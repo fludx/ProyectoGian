69117bf6ca5b103b72a29a4434e70607e9f78494
fludx <rossisantiago724@gmail>
2025-09-12 15:49:20 -0300

    creacion de la vista de admin

--- Citas/Login/Register.php
+++ Citas/Login/Register.php
@@ -3,14 +3,11 @@ require_once "conexion.php";
 
 $conn=conexion();
 
-// Datos del nuevo usuario
 $usuario = $_POST["Usuario"];
 $contrasena = $_POST["Contrasena"];
 
-// Hashear la contraseña antes de enviarla
 $hash = password_hash($contrasena, PASSWORD_DEFAULT);
 
-// Preparar y ejecutar el procedimiento
 $sql="CALL crear_usuario('$usuario','$hash')";
 
 echo mysqli_query($conn,$sql);
